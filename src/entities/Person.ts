3eeeb265b721d07c208b611ea60f345eee3d363f
Diego Chavez <ingdiegochavez18@gmail.com>
2022-05-06 02:05:59 -0500

    👌 IMPROVE: entity-abstraction
    ** Add class abstact Person for Technicals and clients

---
+++ src/entities/Person.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export abstract class Person extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("varchar", { length: 50 })
+    firstName: string;
+
+    @Column("varchar", { length: 50 })
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column("varchar", { length: 15 })
+    phone: string;
+
+    @Column("int", { default: 1 })
+    status: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
